f6e06bf94910b2b98f6ac37d65b0e1f50c391a62
Skauluk <49850186+Skauluk@users.noreply.github.com>
2019-05-16 13:16:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+jQuery(
+function($) {
+	$(".swap").click(function (){
+		$(".side").toggle();
+		return false;
+	});
+	$(".1").click(function (){
+		$('html,body').animate({
+        scrollTop: $(".theme1").offset().top - 10},
+        'slow');
+	});
+	$(".2").click(function (){
+		$('html,body').animate({
+        scrollTop: $(".theme2").offset().top + 30},
+        'slow');
+	});
+	$(".3").click(function (){
+		$('html,body').animate({
+        scrollTop: $(".theme3").offset().top + 30},
+        'slow');
+	});
+	$(".vidos").click(function () {
+		$('iframe').css("z-index",10);
+		$('.hid').css("z-index",10);
+	});
+	$(".hid").click(function () {
+		$('iframe').css("z-index",-10);
+		$('.hid').css("z-index",-10);
+	});
+	$(".side h1 img").click(function () {
+		$('li img').css("width", "65%");
+		$('li img').css("position", "fixed");
+		$('li img').css("top", "-450px");
+		$('li img').css("left", "0px");
+	    $('li img').css("border", "0px");
+	})
+	setTimeout(function() {
+		$(".img1").attr("src","11.jpg");
+	}, 20000);
+	setTimeout(function() {
+		$(".img2").attr("src","12.jpg");
+	}, 40000);
+	setTimeout(function() {
+		$(".img3").attr("src","13.jpg");
+	}, 60000);
+})
